7636d3adfd0501365d3ce935fde6112ff51e5d24
Samix94 <87677220+Samix94@users.noreply.github.com>
2022-03-16 21:32:02 +0100

    Update product.js

--- front/js/product.js
+++ front/js/product.js
@@ -2,6 +2,13 @@ let param = new URLSearchParams(window.location.search);
 const id = param.get('id');
 console.log(id)
 
+let panier = []
+  if (localStorage.getItem("panier")){
+console.log("coucou")
+let panier = JSON.parse(localStorage.getItem("panier"))
+console.log(panier)
+  }
+
 fetch('http://localhost:3000/api/products/' + id)
      .then(function(response) {
       if (response.status !== 200) {
@@ -13,6 +20,7 @@ fetch('http://localhost:3000/api/products/' + id)
      response.json()
       .then(function(data) {
         console.log(data.colors);
+
         let img = document.getElementById("imageUrl")
         img.innerHTML = `<img src="../images/${data.imageUrl}" alt="${data.altTxt}"`
 
@@ -33,11 +41,25 @@ fetch('http://localhost:3000/api/products/' + id)
 
         let description = document.getElementById("description")
         description.innerHTML = `${data.description}`
+        
+        let quantity = document.getElementById("quantity")
+        
+
+        let addToCart = document.getElementById("addToCart")
+        addToCart.addEventListener("click", function(){
+          console.log(id)
+          let addkanap = {
+            _id: id,
+            color: colors.value,
+            qts: quantity.value,
+          }
+          panier.push(addkanap)
+          localStorage.setItem("panier", JSON.stringify(panier))
+
+        })
 
         })
     })
-
-          // Appelez également la fonction qui exécutera automatiquement renderQuote()
 
   .catch(function(err) {
     console.log('Fetch Error :-S', err);
